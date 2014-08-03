fe4b583b44492e60a993b66d112accad8783d06e
Scott Stensland <scottstensland@gmail.com>
2014-08-02 20:04:32 -0400

    modified:   package.json 	modified:   src/app.js 	deleted:    src/app.js~orig.BIG.one 	deleted:    src/index.html~my.own 	deleted:    src/index_websocket_org.html

--- src/app.js
+++ src/app.js
@@ -2,21 +2,24 @@ var WebSocketServer = require("ws").Server
 var http = require("http")
 var express = require("express")
 var app = express()
-var port = process.env.PORT || 5000
+var port = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + "/"))
+app.use(express.static(__dirname + "/"));
 
-var server = http.createServer(app)
-server.listen(port)
+var server = http.createServer(app);
+
+server.listen(port);
 
 console.log("http server listening on %d", port)
 
 var wss = new WebSocketServer({
     server: server
-})
-console.log("websocket server created")
+});
+
+console.log("websocket server created");
 
 wss.on("connection", function(ws) {
+
     var id = setInterval(function() {
         ws.send(JSON.stringify(new Date()), function() {})
     }, 1000)
@@ -27,4 +30,4 @@ wss.on("connection", function(ws) {
         console.log("websocket connection close")
         clearInterval(id)
     })
-})
+});
