111af386d6edbad4061dd5a4068f589150c1628c
Scott Stensland <scottstensland@gmail.com>
2019-12-25 10:22:03 -0500

    modified:   src/toy_ws_client.js

--- src/toy_ws_client.js
+++ src/toy_ws_client.js
@@ -2,8 +2,16 @@
 
 const WebSocket = require('ws');
  
+
+const ws_port = 8888
+
+var ws_url = 'ws://localhost:' + ws_port
+
+console.log("\nhere is ws_url ", ws_url, "\n\n");
+
 // const ws = new WebSocket('ws://www.host.com/path');
-const ws = new WebSocket('ws://localhost:8080');
+// const ws = new WebSocket('ws://localhost:8080');
+const ws = new WebSocket( ws_url );
  
 ws.on('open', function open() {
   const array = new Float32Array(5);
